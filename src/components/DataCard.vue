0c1684a97e5ef8490eebd654f49a29c3ab8f2d8a
Marcello Lopes <marcell0lopes@outlook.com>
2022-02-28 11:23:03 -0300

    data cards

---
+++ src/components/DataCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <div class="shadow-md bg-slate-700 p-10 text-center rounded text-white/95">
+      <h3 class="text-2xl font-bold mb-4">Cases</h3>
+      <div class="text-lg mb-4">
+        <h4 class="font-bold mb-2">
+          New:
+          <span class="font-normal text-emerald-500">{{
+            formatNumber(stats.NewConfirmed)
+          }}</span>
+        </h4>
+
+        <h4 class="font-bold">
+          Total:
+          <span class="font-normal text-emerald-500">{{
+            formatNumber(stats.TotalConfirmed)
+          }}</span>
+        </h4>
+      </div>
+    </div>
+
+    <div class="shadow-md bg-slate-900 p-10 text-center rounded text-white/95">
+      <h3 class="text-2xl font-bold mb-4">Deaths</h3>
+      <div class="text-lg mb-4">
+        <h4 class="font-bold mb-2">
+          New:
+          <span class="font-normal text-emerald-500">{{
+            formatNumber(stats.NewDeaths)
+          }}</span>
+        </h4>
+
+        <h4 class="font-bold">
+          Total:
+          <span class="font-normal text-emerald-500">{{
+            formatNumber(stats.TotalDeaths)
+          }}</span>
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataCard',
+  props: ['stats'],
+  methods: {
+    formatNumber(number) {
+      return number.toLocaleString('pt-BR');
+    },
+  },
+};
+</script>
