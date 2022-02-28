275a5211f55a5a2a096ce5004ccc0575bd961630
Marcello Lopes <marcell0lopes@outlook.com>
2022-02-28 10:39:44 -0300

    App Header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="text-center text-white bg-blue-700 p-4 mb-10">
+    <div class="text-2xl md:text-4xl font-bold mb-3">
+      <i class="fa fa-virus" />
+      COVID-19 TRACKER
+    </div>
+    <p>
+      API by
+      <a
+        href="https://covid19api.com/"
+        target="_blank"
+        class="text-emerald-500 hover:underline font-semibold"
+        >Covid19 API</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
